6381619f97184ec3528445e8a9929c45feb72423
George Rayten Arnold Rex <grex2595@gmail.com>
2021-07-02 11:32:13 -0400

    Make bot environment-specific
    Now the bot can be a test bot or a live bot. The live bot will respond
    to the commands normally. The test bot will only respond to commands
    that start with "test". Also made commands not case-sensitive. Added
    notification that bot started up, so I can know when the bot went down
    for whatever reason.

--- gamble-bot/app.ts
+++ gamble-bot/app.ts
@@ -1,4 +1,4 @@
-import { Client, Message } from 'discord.js'
+import { Client, Message, TextChannel } from 'discord.js'
 import { CurrencyService } from './currency/currency.service';
 import { StorageService } from './storage/storage.service';
 
@@ -6,6 +6,8 @@ const client = new Client();
 let userId: string;
 
 const OWNER = process.env.OWNER || '';
+const ENVIRONMENT = process.env.ENVIRONMENT ? process.env.ENVIRONMENT.toLocaleLowerCase() : process.exit();
+const IS_LIVE = ENVIRONMENT === 'live';
 
 const config = require('./config/config.json');
 
@@ -14,17 +16,30 @@ const currencyService = new CurrencyService(storageService, client);
 
 client.on('ready', () => {
     console.log('Bot ready');
+    if (config.sendStartupMessage) {
+        client.users.fetch(OWNER).then(owner => {
+            owner.send("Bot has started");
+        });
+    }
     userId = client.user && client.user.id ? client.user.id : '';
 });
 
 client.on('message', msg => {
-    if (msg.content === 'my balance') {
+    let content = msg.content.toLocaleLowerCase();
+    if (!IS_LIVE) {
+        if (content.startsWith(ENVIRONMENT)) {
+            content = content.substring(ENVIRONMENT.length + 1);
+        } else {
+            return;
+        }
+    }
+    if ('my balance'.localeCompare(content) === 0) {
         if (!msg.guild || !msg.member) {
             return;
         }
         const rewards = currencyService.getRewards(msg.guild, msg.member);
         msg.reply(`your balance is ${rewards} stakes`);
-    } else if (msg.content === 'bot save' && msg.member && msg.member.id === OWNER) {
+    } else if ('bot save'.localeCompare(content) === 0 && msg.member && msg.member.id === OWNER) {
         currencyService.storeRewards().then(val => {
             msg.reply('all currencies saved');
         });
@@ -40,12 +55,18 @@ client.login(process.env.BOT_TOKEN);
 
 function stop() {
     currencyService.storeRewards().then(() => {
-        client.users.fetch(OWNER)
-            .then(owner => owner.send('Rewards stored. Bot shutting down'))
-            .then(() => process.exit());
-    });
+        return client.users.fetch(OWNER)
+            .then(owner => {
+                if (config.sendExitMessage) {
+                    return owner.send('Rewards stored. Bot shutting down');
+                } else {
+                    return null;
+                }
+            })
+    })
+        .then(() => process.exit());
 }
 
-process.on('SIGINT', stop);
+process.once('SIGINT', stop);
 
-process.on('SIGTERM', stop);
+process.once('SIGTERM', stop);
